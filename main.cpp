33335266607dc96ad91814d8faeef2467ff5422a
Razvan-George Gabreanu <gabreanur@gmail.com>
2025-04-12 10:58:38 +0300

    Created the mainMenu function

--- main.cpp
+++ main.cpp
@@ -1,48 +1,88 @@
 #include <iostream>
-#include <array>
+#include <fstream>
+
+using namespace std;
+
+void mainMenu() {
+    while (true) {
+        cout << endl << "MENU:" << endl;
+        cout << "1. Normal Mode" << endl;
+        cout << "2. Demo Mode" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Enter your choice: ";
+
+        char choice;
+        cin >> choice;
+
+        switch (choice) {
+            case '1':
+                cout << "Entering Normal Mode" << endl;
+            return;
+            case '2':
+                cout << "Entering Demo Mode" << endl;
+            return;
+            case '3':
+                cout << "Exiting Program" << endl;
+            exit(0);
+            default:
+                cout << "'" << choice << "' is an invalid choice!" << endl;
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
+    mainMenu();
+
     return 0;
 }
+
+// COD INITIAL
+//
+// #include <iostream>
+// #include <array>
+//
+// int main() {
+//     std::cout << "Hello, world!\n";
+//     std::array<int, 100> v{};
+//     int nr;
+//     std::cout << "Introduceți nr: ";
+//     /////////////////////////////////////////////////////////////////////////
+//     /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
+//     /// dați exemple de date de intrare folosind fișierul tastatura.txt
+//     /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
+//     /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
+//     /// De asemenea, trebuie să adăugați în acest fișier date de intrare
+//     /// pentru cât mai multe ramuri de execuție.
+//     /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
+//     /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
+//     ///
+//     /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
+//     /// pentru a simula date introduse de la tastatură.
+//     /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
+//     ///
+//     /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
+//     /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
+//     /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
+//     /// program care merg (și să le evitați pe cele care nu merg).
+//     ///
+//     /////////////////////////////////////////////////////////////////////////
+//     std::cin >> nr;
+//     /////////////////////////////////////////////////////////////////////////
+//     for(int i = 0; i < nr; ++i) {
+//         std::cout << "v[" << i << "] = ";
+//         std::cin >> v[i];
+//     }
+//     std::cout << "\n\n";
+//     std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
+//     for(int i = 0; i < nr; ++i) {
+//         std::cout << "- " << v[i] << "\n";
+//     }
+//     ///////////////////////////////////////////////////////////////////////////
+//     /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
+//     /// alt fișier propriu cu ce alt nume doriți.
+//     /// Exemplu:
+//     /// std::ifstream fis("date.txt");
+//     /// for(int i = 0; i < nr2; ++i)
+//     ///     fis >> v2[i];
+//     return 0;
+// }
